d92b151c47993f6632ad1b91412b536946019d00
vishalyadav0507 <vishal364364yadav>
2022-12-17 14:10:25 +0530

    CreatingNodesandModifyingDom

--- node.js
+++ node.js
@@ -6,10 +6,98 @@ var itemlist=document.querySelector('#items');
 // console.log(itemlist.parentElement.parentElement)
 
 
+
 // lastelementchild
 // itemlist.lastElementChild.textContent='hello 4'
 // console.log(itemlist.lastElementChild.innerHTML)
 
+
+
 // lastchild
-console.log(itemlist.lastChild)
+// console.log(itemlist.lastChild)
+
+
+
+//createChildClass
+// newChildClass=document.querySelector('.list-group')
+// var text=document.createTextNode('hello')
+// newChildClass.appendChild(text)
+// document.getElementById("items").appendChild(newChildClass);
+
+
+
+// firstelementchild
+// console.log(itemlist.firstElementChild);
+// itemlist.firstElementChild.textContent='hello 1';
+
+
+
+//firstchild
+// console.log(itemlist.firstChild)
+
+
+
+//nextsibling
+// console.log(itemlist.nextSibling)
+
+
+
+// nextelementsibling
+// console.log(itemlist.nextElementSibling)
+
+
+
+//previoussibling
+// console.log(itemlist.previousSibling)
+
+
+
+// previousElementSibling
+// console.log(itemlist.previousElementSibling)
+
+
+
+// createelement
+// var newEelement=document.createElement('div')
+// newEelement.className='newClass'
+
+
+
+// set attributes
+// newEelement.setAttribute('title','helo')
+
+
+// textnode
+// var newEelementText=document.createTextNode('newNode');
+
+
+
+// append child
+// newEelement.appendChild(newEelementText)
+// console.log(newEelement)
+
+
+
+// Now go head and add HEllo word before Item Lister
+// h6=document.createElement('h6')
+// h6.className="header1"
+// var text=document.createTextNode('hello')
+// h6.appendChild(text)
+// console.log(h6)
+// container=document.querySelector('header .container')
+// head=document.querySelector('header h1')
+// container.insertBefore(h6, head)
+
+
+
+
+//Now go head and add HEllo word before Item 1
+// ParentNode=document.getElementById('items')
+// list=document.createElement('li')
+// list.className="list-group-item"
+// var textlist=document.createTextNode('hello world')
+// list.appendChild(textlist)
+// console.log(list)
+
+// ParentNode.innerHTML=list.innerHTML+ParentNode.innerHTML
 
