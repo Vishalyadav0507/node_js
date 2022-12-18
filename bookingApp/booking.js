01c75e65eca391f7c433d09b40a3680aba238bbe
vishalyadav0507 <vishal364364yadav>
2022-12-18 14:17:54 +0530

    Lets Scale the app to more users

--- bookingApp/booking.js
+++ bookingApp/booking.js
@@ -1,13 +1,20 @@
 var form = document.getElementById('my-form')
 form.addEventListener('submit',onsubmit);
-function onsubmit(e){
+function onsubmit(e){  
+    e.preventDefault()
     var Name = document.getElementById('name').value;
-    var email =document.getElementById('email').value
+    var email =document.getElementById('email').value;
     var phone =document.getElementById('phone').value;
     const obj={
-        Name: Name,
+        Name:Name,
         email:email,
-        phone:phone
+        phone:phone,
     }
-    localStorage.setItem(Name,JSON.stringify(obj))
+    localStorage.setItem(email,JSON.stringify(obj))
+    callingList(obj)
+}
+function callingList(user){
+    const perentNode=document.getElementById('show')
+    const childHTML =`<li> ${user.Name} ${user.email}</li>`
+    perentNode.innerHTML = perentNode.innerHTML+childHTML;
 }
