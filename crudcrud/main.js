7f11c983e475952c866ee65a3c893c1f5d00d8a7
vishalyadav0507 <vishal364364yadav>
2023-01-17 10:17:52 +0530

    get_details_from_crudcrud

--- crudcrud/main.js
+++ crudcrud/main.js
@@ -14,7 +14,22 @@ function addpost(e){
         'phone':number
     }
     axios
-        .post('https://crudcrud.com/api/9a77202def5a46d5b4057feb3a25d645/StudentData',obj)
-    .then((response)=>{console.log(response)})
+        .post('https://crudcrud.com/api/6ec7e0a833294cdfab1f5c1dda39b933/StudentData',obj)
+    .then((response)=>{ showOnScreen(response.data)})
+
 }
 
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.get('https://crudcrud.com/api/6ec7e0a833294cdfab1f5c1dda39b933/StudentData')
+    .then((response)=>{console.log(response)
+        for (var i=0; i<response.data.length;i++){
+            showOnScreen(response.data[i])
+        }
+    })
+})
+
+function showOnScreen(user){
+    const perentNode=document.getElementById('show')
+    const childHTML =`<li> ${user.Name} ${user.Email} </li>`
+    perentNode.innerHTML = perentNode.innerHTML+childHTML;
+}
